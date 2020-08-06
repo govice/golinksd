720552c4b61cb54a8825bab23675cf1cf2be8c98
Kevin Gentile <kgentile@protonmail.com>
2020-08-06 18:54:53 -0400

    add config file, add local ledger

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 
 	maddr "github.com/multiformats/go-multiaddr"
+	"github.com/spf13/viper"
 
 	"github.com/kardianos/service"
 )
@@ -19,6 +24,17 @@ type Config struct {
 }
 
 func main() {
+	if err := setupConfig(); err != nil {
+		fatalln(err)
+	}
+
+	l, err := loadLedger()
+	if err != nil {
+		fatalln(err)
+	}
+	ledger = *l
+	logln(ledger)
+
 	log.Println("DOCKER_MACHINE_IP: " + os.Getenv("DOCKER_MACHINE_IP"))
 	log.Println("PORT: " + os.Getenv("PORT"))
 	log.Println("AUTH_SERVER: " + os.Getenv("AUTH_SERVER"))
@@ -47,4 +63,53 @@ func main() {
 	if err != nil {
 		daemonLogger.Error(err)
 	}
+}
+
+func setupConfig() error {
+	daemonHome := HomeDir()
+
+	os.Mkdir(daemonHome, os.ModePerm)
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.SetDefault("peerPort", 7777)
+
+	viper.AddConfigPath(daemonHome)
+
+	logln("reading config")
+	err := viper.ReadInConfig()
+	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+		configFile := filepath.Join(daemonHome, "config.json")
+		if _, err := os.Create(configFile); err != nil {
+			return err
+		}
+		logln("creating new config file")
+		if err := viper.WriteConfig(); err != nil {
+			logln("failed to write new config file")
+			return err
+		}
+	}
+	return nil
+}
+
+var ErrInvalidLedger = errors.New("failed to load ledger from config")
+
+func loadLedger() (*Ledger, error) {
+	ledger := &Ledger{}
+
+	ledgerBytes, err := ioutil.ReadFile(filepath.Join(HomeDir(), "ledger.json"))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(ledgerBytes, ledger); err != nil {
+		return nil, err
+	}
+
+	return ledger, nil
+}
+
+func HomeDir() string {
+	homeDir, _ := os.UserHomeDir()
+	return filepath.Join(homeDir, ".golinks-daemon")
 }
