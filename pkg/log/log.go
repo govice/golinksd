cda3ee6ec6cf43b1dbefcd54fc21c53a7f241b80
Kevin Gentile <kgentile@protonmail.com>
2020-10-31 10:21:09 -0400

    move scheduler and log to pkg

---
+++ pkg/log/log.go
@@ -0,0 +1,31 @@
+package log
+
+import (
+	"log"
+	"os"
+)
+
+var stdOutLogger = log.New(os.Stdout, "", log.Ltime)
+var logLogger = log.New(os.Stderr, "golinksd LOG: ", log.Ltime)
+var errorLogger = log.New(os.Stderr, "golinksd ERROR: ", log.Ltime)
+
+func Println(v ...interface{}) {
+	stdOutLogger.Println(v...)
+}
+
+func Logln(v ...interface{}) {
+	logLogger.Println(v...)
+}
+
+func Logf(format string, v ...interface{}) {
+	logLogger.Printf(format, v...)
+}
+
+func Errln(v ...interface{}) {
+	errorLogger.Println(v...)
+}
+
+func Fatalln(v ...interface{}) {
+	stdOutLogger.Println(v...)
+	os.Exit(1)
+}
